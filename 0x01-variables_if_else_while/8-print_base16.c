26fc63d4ea0187980b9bc8c5e05810df2452b2c4
yassmine <yassmine.elghalbzouri129@gmail.com>
2023-03-17 10:37:33 -0700

    My first commit

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+    int i;
+
+    for (i = 0; i < 16; i++) {
+        if (i < 10) {
+            putchar(i + '0');
+        } else {
+            putchar(i - 10 + 'a');
+        }
+    }
+
+    putchar('\n');
+
+    return 0;
+}
