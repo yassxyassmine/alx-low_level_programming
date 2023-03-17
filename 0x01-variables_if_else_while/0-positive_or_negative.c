919d5d808b9948a4b615118bfeba6f0dce09ae00
yassmine <yassmine.elghalbzouri129@gmail.com>
2023-03-17 08:11:11 -0700

    My first commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,20 +3,15 @@
 #include <time.h>
 
 int main() {
-    int n;
-    
-    srand(time(NULL));  // seed the random number generator
-    n = rand() % 201 - 100;  // generate a random number between -100 and 100
-    
-    printf("The number %d is ", n);
-    
+    srand(time(NULL)); 
+    int n = rand() % 201 - 100;
+    printf("%d is ", n);
     if (n > 0) {
-        printf("positive.\n");
+        printf("positive\n");
     } else if (n == 0) {
-        printf("zero.\n");
+        printf("zero\n");
     } else {
-        printf("negative.\n");
+        printf("negative\n");
     }
-    
     return 0;
 }
