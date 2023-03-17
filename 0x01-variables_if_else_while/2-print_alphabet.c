bc50ccc76d0e6957c58030bf166c5aada084e9a8
yassmine <yassmine.elghalbzouri129@gmail.com>
2023-03-17 09:20:37 -0700

    My first commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+    int i;
+
+    for (i = 97; i < 123; i++)
+    {
+        putchar(i);
+    }
+    putchar('\n');
+
+    return (0);
+}
