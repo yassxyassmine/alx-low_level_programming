2ce50e21d380213390014cfe59521c9dc8182331
yassmine <yassmine.elghalbzouri129@gmail.com>
2023-03-17 10:54:50 -0700

    my first commit

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main() {
+    int i, j;
+    for(i=0; i<100; i++) {
+        for(j=i; j<100; j++) {
+            if(i!=j) {
+                putchar(i/10+'0');
+                putchar(i%10+'0');
+                putchar(' ');
+                putchar(j/10+'0');
+                putchar(j%10+'0');
+                if(i!=98 || j!=99) {
+                    putchar(',');
+                    putchar(' ');
+                }
+            }
+        }
+    }
+    putchar('\n');
+    return 0;
+}
