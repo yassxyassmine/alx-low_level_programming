55a8f340c2166c8a56ca0d48d6bb2033817ea030
yassmine <yassmine.elghalbzouri129@gmail.com>
2023-03-20 23:37:54 -0700

    My first commit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 int print_last_digit(int n) {
-    int last_digit = abs(n) % 10;
-    printf("%d", last_digit); 
-    return last_digit; 
+    int last_digit = n % 10;
+    if (last_digit < 0) {
+        last_digit *= -1;
+    }
+    printf("%d", last_digit);
+    return last_digit;
 }
 
-int main() {
-    int num = 12345;
+int main(void) {
+    int num = 123456789;
     int last_digit = print_last_digit(num);
-    printf("\nThe last digit of %d is %d", num, last_digit);
+    printf("\nLast digit of %d is %d\n", num, last_digit);
     return 0;
 }
