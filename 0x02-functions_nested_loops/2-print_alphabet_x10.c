81bef9273b7e57d6446f0547a0fdc1867daffdca
yassmine <yassmine.elghalbzouri129@gmail.com>
2023-03-22 08:17:47 -0700

    My first commit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,19 +1,17 @@
-#include <stdio.h>
+#include "main.h"
 
-void print_alphabet_x10(void) {
-    char letter;
+void print_alphabet_x10(void)
+{
+    char c;
     int i;
-    for (i = 0; i < 10; i++) {
-        letter = 'a';
-        while (letter <= 'z') {
-            putchar(letter);
-            letter++;
+    for (i = 0; i < 10; i++)
+    {
+        c = 'a';
+        while (c <= 'z')
+        {
+            _putchar(c);
+            c++;
         }
-        putchar('\n');
+        _putchar('\n');
     }
-}
-
-int main() {
-    print_alphabet_x10();
-    return 0;
 }
